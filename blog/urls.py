eb03a9e46610d142f3976033bccf8d6096ae0536
ChoriyevJamshid <jamshidchoriyev709@gmail.com>
2026-06-17 19:42:36 +0500

    updated

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('', views.index_view, name='index-view'),
+    path('', views.post_list_view,
+         name='post-list-view'),
+
+    path('<int:category_id>/', views.post_list_view,
+         name='post-list-view-category'),
 ]
-
-
-
--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,32 @@
 from django.shortcuts import render
 
-from blog.models import Post
+from blog.models import Post, Category
 
 
+def post_list_view(request, category_id=None):
+    categories = Category.objects.all()
+    posts = Post.objects.filter(is_published=True)
 
-def index_view(request):
+    if category_id is None:
+        category_id = (
+            Category.objects
+            .values_list('id', flat=True)
+            .first())
 
-    main_post = (
-        Post.objects
-        .filter(is_published=True)
-        .order_by('-published_at').first()
-    )
+    posts = posts.filter(category__id=category_id)
 
-    posts = (
-        Post.objects
-        .filter(is_published=True)
-        .exclude(id=main_post.pk)
-    )
+    main_post = posts[0] if posts else None
+    posts = posts.exclude(id=main_post.pk if main_post else 0)
 
     context = {
+        'categories': categories,
+        'posts': posts,
         'main_post': main_post,
-        'posts': posts
+        'selected_category': category_id,
     }
+
+    # print(context)
 
     return render(request,
                   template_name='blog/index.html',
                   context=context)
-
-
-
